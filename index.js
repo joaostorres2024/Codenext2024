2542ef9d06276e0a36cf12a154148d68fdf1cb83
joaostorres2024 <joao.pedrost2004@gmail.com>
2024-12-02 13:41:06 -0300

    .

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function alerta(){
     alert("Ainda não disponível!")
 }
 
+<<<<<<< HEAD
 window.addEventListener("mousemove", e => {
     let x = e.clientX;
     let y = e.clientY;
@@ -14,4 +15,12 @@ window.addEventListener("mousemove", e => {
     section.style.backgrounPOsitionX = `-${x * 0.09}px`;
     section.style.backgrounPOsitionY = `-${x * 0.09}px`;
 
+=======
+    const rain = document.querySelectorAll(".rain");
+    const getRandomNumber = (min, max)=>{
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+    rain.forEach(rain =>{
+    rain.style = `--rain-delay:${getRandomNumber(0, 1000)}ms`;
+>>>>>>> 49c17c0 (Final)
 })
